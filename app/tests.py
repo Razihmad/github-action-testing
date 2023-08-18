1d0e0521fe35ab83912115993278ee003e3b15c0
Razi Ahmad <mail2razi4u@gmail.com>
2023-08-18 13:46:06 +0530

    update code

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Blog
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.blog = Blog.objects.create(title='My Blog Title', author='My Name', slug='my-blog-title')
+
+    def test_blog_content(self):
+        blog = Blog.objects.get(id=1)
+        expected_object_name = f'{blog.title}'
+        self.assertEquals(expected_object_name, 'My Blog Title')
